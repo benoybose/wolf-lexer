b3bcdd049f39f1c83ed635f6377a5bec22f72d02
Benoy Bose <benoybose@gmail.com>
2016-05-13 14:52:20 +0000

    Added istanbul for coveralls

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ var plugins = require('gulp-load-plugins')();
 var paths = {
   lint: ['./lib/**/*.js'],
   watch: ['./gulpfile.js', './lib/**', './test/**/*.js', '!test/{temp,temp/**}'],
-  tests: ['./test/**/*.js', '!test/{temp,temp/**}']
+  tests: ['./test/**/*.js', '!test/{temp,temp/**}'],
+  source: ['./lib/*.js', './lib/**/*.js']
 };
 
 var plumberConf = {};
@@ -31,6 +32,22 @@ gulp.task('unitTest', function () {
     .pipe(plugins.mocha({ reporter: 'list' }));
 });
 
+gulp.task('istanbul', function (cb) {
+  gulp.src(paths.source)
+    .pipe(plugins.istanbul()) // Covering files
+    .pipe(plugins.istanbul.hookRequire()) // Force `require` to return covered files
+    .on('finish', function () {
+      gulp.src(paths.tests)
+        .pipe(plugins.plumber(plumberConf))
+        .pipe(plugins.mocha())
+        .pipe(plugins.istanbul.writeReports()) // Creating the reports after tests runned
+        .on('finish', function() {
+          process.chdir(__dirname);
+          cb();
+        });
+    });
+});
+
 gulp.task('bump', ['test'], function () {
   var bumpType = plugins.util.env.type || 'patch'; // major.minor.patch
 
@@ -43,7 +60,7 @@ gulp.task('watch', ['test'], function () {
   gulp.watch(paths.watch, ['test']);
 });
 
-gulp.task('test', ['lint', 'unitTest']);
+gulp.task('test', ['lint', 'istanbul']);
 
 gulp.task('release', ['bump']);
 
